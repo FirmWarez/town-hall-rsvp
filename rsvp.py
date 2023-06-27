01c34c8e4cd611df308d70e793737fc2f651ad81
firmwarez <firmwarez@firmwarez.com>
2023-06-27 17:46:59 -0500

    Correcting bug causing some lines in city file to be skipped

--- rsvp.py
+++ rsvp.py
@@ -4,16 +4,17 @@ def main():
 
     cities_file=open("cities.txt", "r")
     for line in cities_file:
-        city = cities_file.readline()
+        city = line
         city = city.strip()
+        print("trying " + city)
         url = "https://governor.arkansas.gov/" + city + "-town-hall-rsvp/"
         try:
             r = requests.get(url)
             # print(url + "\tStatus: " + str(r.status_code))
             if(r.status_code != 404):
                 print(url + " is a valid RSVP link")
-            else:
-                print(city + " does not have a valid RSVP link")
+            # else:
+            #     print(city + " does not have a valid RSVP link")
         except Exception as e:
             print(url + "\tNA FAILED TO CONNECT\t" + str(e))
 
